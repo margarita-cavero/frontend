442e9a7781249a4f4d8b7eca8653e512e3f7f498
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-21 01:58:44 +0200

    gh-pages

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/envsh
+
+# abortar en caso de errores
+set -e
+
+# compilado
+npm run build
+
+# navega al directorio de salida de compilación
+dist cd
+
+# si estás desplegando en un dominio personalizado
+# echo 'www.ejemplo.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# si estás desplegando en https://<NOMBRE DE USUARIO>.github.io
+# git push -f git@github.com:<NOMBRE DE USUARIO>/<NOMBRE DE USUARIO>.github.io.git main
+
+# si estás desplegando en https://<NOMBRE DE USUARIO>.github.io/<REPO>
+# git push -f git@github.com:margarita-cavero/frontend.git main:gh-pages
+
+cd -
